15e89d4e824040918770588d04ef49d6358d09ce
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2023-03-27 17:02:38 -0600

    Create first draft of initialize method in Game class

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,8 +1,17 @@
 class Game
     def initialize
-        board = Array.new
-        puts "hello"
+        board = [[1,2,3], [4,5,6], [7,8,9]]
+        puts "Welcome to Tic Tac Toe! Let's play a game in the console."
+        puts "What is player 1's name?" 
+        @player_1_name = gets
+        puts "What is player 2's name?" 
+        @player_2_name = gets
+        p board
     end
+
+    def get_player_selection(number)
+    end
+
 end
 
 newGame = Game.new
