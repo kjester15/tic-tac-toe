2ab88ac9a0e20accf0fbef37d079f57a2e3c872e
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2023-03-28 17:38:35 -0600

    Put all game functionality into prelim loop

--- tictactoe.rb
+++ tictactoe.rb
@@ -43,34 +43,45 @@ class Player
     end
 end
 
-new_game = Game.new
-
-puts "Welcome to Tic Tac Toe! Let's play a game in the console."
-
-# create player 1 class
-puts "What is player 1's name?" 
-player_name = gets.chomp
-puts "What letter/symbol would you like to use for your game marker, #{player_name}?" 
-player_symbol = gets.chomp
-player1 = Player.new(player_name, player_symbol)
-
-# create player 2 class
-puts "What is player 2's name?" 
-player_name = gets.chomp
-puts "What letter/symbol would you like to use for your game marker, #{player_name}?"
-player_symbol = gets.chomp
-player2 = Player.new(player_name, player_symbol)
-
-puts player1.get_name_symbol
-puts player2.get_name_symbol
-puts new_game.print_board
-
 while not game_finished
-    player1.make_selection
-    new_game.check_win
-    player2.make_selection
-    new_game.check_win
-    game_finished = true
+    # initialize new game
+    new_game = Game.new
+    puts "Welcome to Tic Tac Toe! Let's play a game in the console."
+    
+    # create player 1 class
+    puts "What is player 1's name?" 
+    player_name = gets.chomp
+    puts "What letter/symbol would you like to use for your game marker, #{player_name}?" 
+    player_symbol = gets.chomp
+    player1 = Player.new(player_name, player_symbol)
+    
+    # create player 2 class
+    puts "What is player 2's name?" 
+    player_name = gets.chomp
+    puts "What letter/symbol would you like to use for your game marker, #{player_name}?"
+    player_symbol = gets.chomp
+    player2 = Player.new(player_name, player_symbol)
+    
+    # print player names and symbols for error checking - CAN EVENTUALLY DELETE
+    puts player1.get_name_symbol
+    puts player2.get_name_symbol
+    puts new_game.print_board
+
+    # loop and let players make choices until game is over
+    while not game_finished
+        # first player makes selection, then check if game is over
+        player1.make_selection
+        new_game.check_win
+        # if the game isn't over, player 2 can select, then check if game is over
+        if not game_finished
+            player2.make_selection
+            new_game.check_win
+        end
+    end
+
+    # calculate and return the winner
+
+    # ask to play another game
 end
 
 
