cc738353112ab86c8558aeb7a6fdad4a7eca2af8
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2023-10-30 16:02:20 -0700

    Complete tests for tic tac toe

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -1,6 +1,6 @@
 # the Game class represents the current game of Tic Tac Toe
 class Game
-  attr_accessor :board, :winner, :player_turn, :win, :game_finished, :draw
+  attr_accessor :board, :winner, :player_turn, :win, :game_finished, :draw, :board_array
 
   def initialize
     @board = " 1 | 2 | 3 \n---+---+---\n 4 | 5 | 6 \n---+---+---\n 7 | 8 | 9 "
@@ -72,78 +72,4 @@ class Player
     end
     @selection
   end
-end
-
-while
-    # initialize new game - DONE
-    new_game = Game.new
-    puts "Welcome to Tic Tac Toe! Let's play a game in the console."
-
-    # create players - DONE
-    puts "What is player 1's name?"
-    player_name = gets.chomp
-    puts "What letter would you like to use for your game marker, #{player_name}?"
-    player_symbol = gets.chomp
-    until player_symbol.length == 1 && player_symbol.match(/[A-Za-z]/)
-      puts 'Please choose a letter that is only 1 character long, and is a valid alphabetical letter.'
-      player_symbol = gets.chomp
-    end
-    player1 = Player.new(player_name, player_symbol)
-
-    puts "What is player 2's name?"
-    player_name = gets.chomp
-    puts "What letter would you like to use for your game marker, #{player_name}?"
-    player_symbol = gets.chomp
-    until player_symbol.length == 1 && player_symbol.match(/[A-Za-z]/)
-      puts 'Please choose a letter that is only 1 character long, and is a valid alphabetical letter.'
-      player_symbol = gets.chomp
-    end
-    until player_symbol != player1.print_symbol
-      puts 'Please select a different letter/symbol'
-      player_symbol = gets.chomp
-    end
-    player2 = Player.new(player_name, player_symbol)
-
-    # print game board - DONE
-    puts new_game.print_board
-
-    # loop and let players make choices until game is over - DONE
-    while not new_game.game_finished
-      # first player makes selection, then check if game is over
-      new_game.player_turn = player1.name
-      selection = player1.make_selection(player1.name)
-      new_game.replace_number(new_game.board, selection, player1.print_symbol)
-      new_game.check_win(player1.print_symbol)
-      new_game.print_board
-      # if the game isn't over, player 2 can select, then check if game is over
-      if not new_game.game_finished
-        new_game.player_turn = player2.name
-        selection = player2.make_selection(player2.name)
-        new_game.replace_number(new_game.board, selection, player2.print_symbol)
-        new_game.check_win(player2.print_symbol)
-        new_game.print_board
-      end
-    end
-
-    # return the winner - DONE
-    if new_game.draw == true
-      puts "It's a draw!"
-    else
-      new_game.print_winner
-    end
-
-    # ask to play another game - DONE
-    answer = ''
-    until answer == "yes" || answer == "no"
-      puts "Would you like to play again?"
-      answer = gets.chomp.downcase
-      if answer == "yes" || answer == "no"
-        break
-      else
-        puts "Please answer with 'yes' or 'no'"
-      end
-    end
-    if answer == 'no'
-      break
-    end
 end
--- spec/tictactoe_rspec.rb
+++ spec/tictactoe_rspec.rb
@@ -1,9 +1,16 @@
 require_relative '../lib/tictactoe'
-# rspec spec/14_find_number_spec.rb
+# require_relative '../lib/tic_tac_toe_gameplay'
 
 describe Game do
+  subject(:game_main) { described_class.new }
+  # let(:player_one) { instance_double(Player, name: 'kyle', symbol: 'x') }
+
   describe '#initialize' do
     # Initialize -> No test necessary when only creating instance variables.
+    # it 'updates board instance variable with selection' do
+    #   result = " 1 | 2 | 3 \n---+---+---\n 4 | 5 | 6 \n---+---+---\n 7 | 8 | 9 "
+    #   expect(game_main.board).to eq(result)
+    # end
   end
 
   describe '#print_board' do
@@ -11,27 +18,74 @@ describe Game do
   end
 
   describe '#replace_number' do
-    # TODO
     # Command Method -> Test the change in the observable state
-    subject(:game_main) { described_class.new }
-    let(:player_one) { instance_double(Player, name: 'kyle', symbol: 'x') }
-
-    before do
-      allow(@board_array).to receive(:map!)
+    it 'updates board instance variable with selection' do
+      board = " 1 | 2 | 3 \n---+---+---\n 4 | 5 | 6 \n---+---+---\n 7 | 8 | 9 "
+      updated_board = " x | 2 | 3 \n---+---+---\n 4 | 5 | 6 \n---+---+---\n 7 | 8 | 9 "
+      selection = '1'
+      character = 'x'
+      game_main.replace_number(board, selection, character)
+      expect(game_main.instance_variable_get(:@board)).to eq(updated_board)
     end
 
-    it 'updates board instance variable' do
-      board = game_main.instance_variable_get(:@board)
-      selection = 1
-      character = player_one.instance_variable_get(:@symbol)
-      new_board = " x | 2 | 3 \n---+---+---\n 4 | 5 | 6 \n---+---+---\n 7 | 8 | 9 "
-      expect(game_main.board).to eq(new_board)
-      game_main.replace_number(board, selection, character)
+    it 'updates board_array instance variable with selection' do
+      board = " 1 | 2 | 3 \n---+---+---\n 4 | 5 | 6 \n---+---+---\n 7 | 8 | 9 "
+      updated_board_array = ['x', 2, 3, 4, 5, 6, 7, 8, 9]
+      selection = '1'
+      character = 'x'
+      result = game_main.replace_number(board, selection, character)
+      expect(result).to eq(updated_board_array)
     end
   end
 
   describe '#check_win' do
-    # TODO
+    context 'when a player has won' do
+      it 'updates win instance variable' do
+        character = 'x'
+        game_main.board_array = ['x', 'x', 'x', 4, 5, 6, 7, 8, 9]
+        game_main.check_win(character)
+        expect(game_main.instance_variable_get(:@win)).to be true
+      end
+
+      it 'updates game_finished instance variable' do
+        character = 'x'
+        game_main.board_array = ['x', 'x', 'x', 4, 5, 6, 7, 8, 9]
+        game_main.check_win(character)
+        expect(game_main.instance_variable_get(:@game_finished)).to be true
+      end
+    end
+
+    context 'when a player has not won' do
+      it 'does not update win instance variable' do
+        character = 'x'
+        game_main.board_array = ['x', 'x', 3, 4, 5, 6, 7, 8, 9]
+        game_main.check_win(character)
+        expect(game_main.instance_variable_get(:@win)).to be false
+      end
+
+      it 'does not update game_finished instance variable' do
+        character = 'x'
+        game_main.board_array = ['x', 'x', 3, 4, 5, 6, 7, 8, 9]
+        game_main.check_win(character)
+        expect(game_main.instance_variable_get(:@game_finished)).to be false
+      end
+    end
+
+    context 'when the game is a draw' do
+      it 'updates game_finished instance variable' do
+        character = 'x'
+        game_main.board_array = %w[x x y y y x x y x]
+        game_main.check_win(character)
+        expect(game_main.instance_variable_get(:@game_finished)).to be true
+      end
+
+      it 'updates draw instance variable' do
+        character = 'x'
+        game_main.board_array = %w[x x y y y x x y x]
+        game_main.check_win(character)
+        expect(game_main.instance_variable_get(:@draw)).to be true
+      end
+    end
   end
 
   describe '#print_winner' do
@@ -40,6 +94,8 @@ describe Game do
 end
 
 describe Player do
+  subject(:player_main) { described_class.new('kyle', 'x') }
+
   describe '#initialize' do
     # Initialize -> No test necessary when only creating instance variables.
   end
@@ -49,6 +105,17 @@ describe Player do
   end
 
   describe '#make_selection' do
-    # TODO
+    before do
+      allow(player_main).to receive(:gets).and_return('a', '$', '3')
+    end
+
+    it 'loop ends when valid selection is made' do
+      name = 'kyle'
+      initial_prompt = "#{name}, choose a number on the board to make a move."
+      retry_prompt = 'Please choose a valid selection from the remaining numbers on the board.'
+      expect(player_main).to receive(:puts).with(initial_prompt).once
+      expect(player_main).to receive(:puts).with(retry_prompt).twice
+      player_main.make_selection(name)
+    end
   end
 end
